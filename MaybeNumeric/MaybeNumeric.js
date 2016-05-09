4a805416c2f627fed14eeb42315fc714e9796046
Chris Matzenbach <cmatzenabch@gmail.com>
2016-05-09 16:47:57 -0400

    solution?

--- MaybeNumeric/MaybeNumeric.js
+++ MaybeNumeric/MaybeNumeric.js
@@ -1,1 +1,10 @@
-var test = 0;
+function maybeNumeric(input) {
+    if (/ /.test(input))
+        return input.split(' ').map(maybeNumeric);
+    return isNaN(input) ? input : Number(input);
+}
+
+
+console.log(maybeNumeric(123));
+console.log(maybeNumeric(44.234));
+console.log(maybeNumeric(0x123N));
