e419534a45e717c2c5cd4aa6b96b356aa759e232
cmatzenbach <cmatzenbach@gmail.com>
2017-05-03 00:28:42 -0400

    Added list checking; getting type error

--- Subset_Sum/subsetsum.el
+++ Subset_Sum/subsetsum.el
@@ -13,3 +13,15 @@
 
 ; check if any items in the list add up to be 0
 ; loop through each item, checking to see if sum is 0
+(defun check-for-zero (lista listb)
+  (setq check nil)
+  (while lista
+    (while listb
+      (when (= (+ lista listb) 0) (setq check t))
+      (setq listb (cdr listb))
+      (setq lista (cdr lista))))
+  (when check t))
+
+(setq uno '(1 2 3 4))
+(setq dos '(1 -2 3 4))
+(check-for-zero uno dos)
